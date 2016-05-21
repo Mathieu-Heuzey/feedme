9f26c3b13b550b4f4ca5f4fdf453aeafdac1b196
Mathieu-Heuzey <mathieu.heuzey@gmail.com>
2016-05-21 16:00:57 +0200

    finition de la créaation du meal

--- FeedMe/app/src/main/java/mobile/feedme/AddMeal.java
+++ FeedMe/app/src/main/java/mobile/feedme/AddMeal.java
@@ -2,6 +2,7 @@ package mobile.feedme;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -20,27 +21,136 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class AddMeal extends MenuActivity {
 
     public Api api = new Api();
+    static final int TIME_DIALOG_ID = 1111;
+    private TextView output;
+    public Button btnClick;
+
+    private int hour;
+    private int minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.initialize(R.layout.activity_add_meal, true, true);
+        output = (TextView) findViewById(R.id.output);
+
+        /********* display current time on screen Start ********/
+
+        final Calendar c = Calendar.getInstance();
+        // Current Hour
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        // Current Minute
+        minute = c.get(Calendar.MINUTE);
+
+        // set current time into output textview
+        updateTime(hour, minute);
+
+        /********* display current time on screen End ********/
+
+        // Add Button Click Listener
+        addButtonClickListener();
+    }
+
+    public void addButtonClickListener() {
+
+        btnClick = (Button) findViewById(R.id.btnHourPicker);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                showDialog(TIME_DIALOG_ID);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected TimePickerDialog onCreateDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+
+                // set time picker as current time
+                return new TimePickerDialog(this, timePickerListener, hour, minute,
+                        false);
+
+        }
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minutes) {
+            // TODO Auto-generated method stub
+            hour = hourOfDay;
+            minute = minutes;
+
+            updateTime(hour, minute);
+
+        }
+
+    };
+
+    private static String utilTime(int value) {
+
+        if (value < 10)
+            return "0" + String.valueOf(value);
+        else
+            return String.valueOf(value);
+    }
+
+    // Used to convert 24hr format to 12hr format with AM/PM values
+    private void updateTime(int hours, int mins) {
+
+        String timeSet = "";
+        if (hours > 12) {
+            hours -= 12;
+            timeSet = "PM";
+        } else if (hours == 0) {
+            hours += 12;
+            timeSet = "AM";
+        } else if (hours == 12)
+            timeSet = "PM";
+        else
+            timeSet = "AM";
+
+
+        String minutes = "";
+        if (mins < 10)
+            minutes = "0" + mins;
+        else
+            minutes = String.valueOf(mins);
+
+        // Append in a StringBuilder
+        String aTime = new StringBuilder().append(hours).append(':')
+                .append(minutes).append(" ").append(timeSet).toString();
+
+        output.setText(aTime);
     }
 
     public void uploadImage(View view) {
 
-        final CharSequence[] items = { "Take Photo", "Choose from Library",
-                "Cancel" };
+        final CharSequence[] items = {"Take Photo", "Choose from Library",
+                "Cancel"};
         AlertDialog.Builder builder = new AlertDialog.Builder(AddMeal.this);
         builder.setTitle("Add Photo!");
         builder.setItems(items, new DialogInterface.OnClickListener() {
@@ -132,8 +242,7 @@ public class AddMeal extends MenuActivity {
             cursor.close();
             ImageView ImageView = (ImageView) findViewById(R.id.ivImage);
             ImageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
         }
     }
@@ -143,5 +252,9 @@ public class AddMeal extends MenuActivity {
         Bitmap resized = Bitmap.createScaledBitmap(photo, 400, 400, true);
         ImageView ImageView = (ImageView) findViewById(R.id.ivImage);
         ImageView.setImageBitmap(resized);
+    }
+
+    public void saveMeal(View view) {
+
     }
 }
