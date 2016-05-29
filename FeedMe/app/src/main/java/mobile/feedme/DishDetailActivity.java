e4f9574bcd751dc147ed5353d6ad8196b7b1c100
Mathieu-Heuzey <mathieu.heuzey@gmail.com>
2016-05-29 17:27:22 +0200

    ajout du calendrier

--- FeedMe/app/src/main/java/mobile/feedme/DishDetailActivity.java
+++ FeedMe/app/src/main/java/mobile/feedme/DishDetailActivity.java
@@ -1,5 +1,7 @@
 package mobile.feedme;
 
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,11 +9,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import mobile.feedme.POCO.Dish;
 
@@ -23,7 +28,39 @@ public class DishDetailActivity extends MenuActivity {
     private int hour;
     private int minute;
     static final int TIME_DIALOG_ID = 1111;
+    Calendar myCalendar = Calendar.getInstance();
 
+    public void submitDate(View vies)
+    {
+        new DatePickerDialog(this, date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        return;
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    private void updateLabel() {
+
+        String myFormat = "MM-dd"; //In which you need put here
+        Date  tmp = myCalendar.getTime();
+        String fDate = new SimpleDateFormat("MM-dd").format(tmp);
+//        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, tmp);
+        TextView output2 = (TextView) findViewById(R.id.output2);
+        output2.setText("You will pick your dish : " + fDate);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +71,8 @@ public class DishDetailActivity extends MenuActivity {
         Dish dish = i.<Dish>getParcelableExtra("Dish");
         this.dish = dish;
         Log.e("DishDetail: ", dish.toString());
-
+        TextView output2 = (TextView) findViewById(R.id.output2);
+        output2.setText("You will pick your dish : ");
         TextView name = (TextView) findViewById(R.id.textViewDishName);
         name.setText(dish.Name);
 
